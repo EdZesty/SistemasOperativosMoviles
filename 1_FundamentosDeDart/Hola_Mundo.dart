2dda89f6ed5085a427b2af9d9876b01e8baa2b90
EdZesty <116782241+EdZesty@users.noreply.github.com>
2023-09-08 00:42:25 -0600

    Add files via upload

---
+++ 1_FundamentosDeDart/Hola_Mundo.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+//   var myName = 'Eder';
+//   late final myName;
+//   String myName = 'Eder';
+  const myName = 'Eder';
+    
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
